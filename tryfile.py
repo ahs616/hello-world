49500f115ee7fd6fd73f09e0c0859f1d7874cf21
Adam <ahs616@uregina.ca>
2020-05-27 10:41:22 -0600

    New Branch

--- tryfile.py
+++
@@ -1,6 +0,0 @@
-def main(msg):
-    // Print message
-    print(msg)
-
-main("Hello Today People")
-
---
+++ tryfile2.py
@@ -0,0 +1,8 @@
+def main(msg):
+    // Print message
+
+    print(msg)
+
+def secondary ():
+    print ("New Function")
+main("Hello Today People")
